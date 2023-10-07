82a858e0ad058df7e7c51ffa655999f3802e676d
ricky.ltp <ricky.ltp@byteuptech.com>
2023-10-07 18:09:08 +0800

    Init project api-gateway

---
+++ src/test/java/com/phucdevs/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phucdevs.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
